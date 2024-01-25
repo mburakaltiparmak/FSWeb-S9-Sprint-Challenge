1723de3fd08219049cd577d8693639187e26656e
burakaltiparmak <mburakaltiparmak@gmail.com>
2024-01-25 21:30:09 +0300

    Temel aksiyonlar tanımlandı.

--- frontend/components/AppFunctional.js
+++ frontend/components/AppFunctional.js
@@ -1,15 +1,18 @@
-import React from 'react'
+import React, { useState } from "react";
 
 // önerilen başlangıç stateleri
-const initialMessage = ''
-const initialEmail = ''
-const initialSteps = 0
-const initialIndex = 4 //  "B" nin bulunduğu indexi
+const initialMessage = "";
+const initialEmail = "";
+const initialSteps = 0;
+const initialIndex = 4; //  "B" nin bulunduğu indexi
 
 export default function AppFunctional(props) {
   // AŞAĞIDAKİ HELPERLAR SADECE ÖNERİDİR.
   // Bunları silip kendi mantığınızla sıfırdan geliştirebilirsiniz.
-
+  const [index, setIndex] = useState(initialIndex);
+  const [message, setMessage] = useState(initialMessage);
+  const [steps, setSteps] = useState(initialSteps);
+  const [email, setEmail] = useState(initialEmail);
   function getXY() {
     // Koordinatları izlemek için bir state e sahip olmak gerekli değildir.
     // Bunları hesaplayabilmek için "B" nin hangi indexte olduğunu bilmek yeterlidir.
@@ -22,7 +25,10 @@ export default function AppFunctional(props) {
   }
 
   function reset() {
-    // Tüm stateleri başlangıç ​​değerlerine sıfırlamak için bu helperı kullanın.
+    setEmail(initialEmail);
+    setMessage(initialMessage);
+    setSteps(initialSteps);
+    setIndex(initialIndex);
   }
 
   function sonrakiIndex(yon) {
@@ -34,6 +40,55 @@ export default function AppFunctional(props) {
   function ilerle(evt) {
     // Bu event handler, "B" için yeni bir dizin elde etmek üzere yukarıdaki yardımcıyı kullanabilir,
     // ve buna göre state i değiştirir.
+    const yon = evt.target.id;
+    console.log("yön", yon);
+    switch (yon) {
+      case "left":
+        if (index % 3 === 0) {
+          setMessage("Sola gidemezsiniz.");
+        } else {
+          setIndex(index - 1);
+        }
+        break;
+      case "right":
+        if (index % 3 === 2) {
+          setMessage("Sağa gidemezsiniz.");
+        } else {
+          setIndex(index + 1);
+        }
+        break;
+      case "up":
+        if (index < 3) {
+          setMessage("Yukarı gidemezsiniz.");
+        } else {
+          setIndex(index - 3);
+        }
+
+        break;
+      case "down":
+        if (index > 5) {
+          setMessage("Aşağı gidemezsiniz.");
+        } else {
+          setIndex(index + 3);
+        }
+        break;
+    }
+    /*if (yon === "left") {
+      if (index % 3 === 0) {
+        setMessage("Sola gidemezsiniz.");
+      } else {
+        setIndex(index - 1);
+      }
+    }
+    if (yon === "right") {
+      setIndex(index + 1);
+    }
+    if (yon === "up") {
+      setIndex(index - 3);
+    }
+    if (yon === "down") {
+      setIndex(index + 3);
+    }*/
   }
 
   function onChange(evt) {
@@ -48,31 +103,44 @@ export default function AppFunctional(props) {
     <div id="wrapper" className={props.className}>
       <div className="info">
         <h3 id="coordinates">Koordinatlar (2, 2)</h3>
-        <h3 id="steps">0 kere ilerlediniz</h3>
+        <h3 id="steps">{steps} kere ilerlediniz</h3>
       </div>
       <div id="grid">
-        {
-          [0, 1, 2, 3, 4, 5, 6, 7, 8].map(idx => (
-            <div key={idx} className={`square${idx === 4 ? ' active' : ''}`}>
-              {idx === 4 ? 'B' : null}
-            </div>
-          ))
-        }
+        {[0, 1, 2, 3, 4, 5, 6, 7, 8].map((idx) => (
+          <div key={idx} className={`square${idx === index ? " active" : ""}`}>
+            {idx === index ? index : idx}
+          </div>
+        ))}
       </div>
       <div className="info">
-        <h3 id="message"></h3>
+        <h3 id="message"> {message}</h3>
       </div>
       <div id="keypad">
-        <button id="left">SOL</button>
-        <button id="up">YUKARI</button>
-        <button id="right">SAĞ</button>
-        <button id="down">AŞAĞI</button>
-        <button id="reset">reset</button>
+        <button onClick={ilerle} id="left">
+          SOL
+        </button>
+        <button onClick={ilerle} id="up">
+          YUKARI
+        </button>
+        <button onClick={ilerle} id="right">
+          SAĞ
+        </button>
+        <button onClick={ilerle} id="down">
+          AŞAĞI
+        </button>
+        <button id="reset" onClick={reset}>
+          reset
+        </button>
       </div>
       <form>
-        <input id="email" type="email" placeholder="email girin"></input>
+        <input
+          value={email}
+          id="email"
+          type="email"
+          placeholder="email girin"
+        ></input>
         <input id="submit" type="submit"></input>
       </form>
     </div>
-  )
+  );
 }
